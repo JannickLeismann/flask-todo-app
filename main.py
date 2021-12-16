f9cd829b62e070721bef23c8de08eee9a313c693
JannickL <jannick.leismann@gmx.de>
2021-12-16 14:25:02 +0100

    commit main files

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from models import Schema
+from todoService import TodoService
+
+
+app = Flask(__name__) #__main__
+
+@app.route("/todo",methods = ["POST"])
+def create_todo():
+    req = request.get_json()
+    print(req)
+    return TodoService().create(req)
+
+@app.route("/todo", methods = ["GET"])
+def get_todos():
+    return  jsonify(TodoService().get_all())
+
+
+if __name__ == "__main__":
+    Schema()
+    app.run(debug = True)
---
+++ models.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+class Schema:
+    def __init__(self):
+        self.conn = sqlite3.connect('todo.db')
+        self.create_todo_table()
+
+    def create_todo_table(self):
+        query = """
+            CREATE TABLE IF NOT EXISTS "Todo" (
+                id INTEGER PRIMARY KEY,
+                Title TEXT,
+                Description TEXT            
+            );
+        """
+
+        self.conn.execute(query)
+
+
+class TodoModel:
+
+    def __init__(self):
+        self.conn = sqlite3.connect('todo.db')
+
+    def create(self, title, description):
+        query = f'insert into Todo (Title, Description) values ("{title}","{description}")'
+
+        result = self.conn.execute(query)
+        self.conn.commit()
+
+        return f"Ok {result.lastrowid}"
+
+    def get_all(self):
+        query = "select * from Todo"
+        result = self.conn.execute(query).fetchall()
+        return result
